99c3a313ecef42abb06539d0dab303e6d02b7f50
Franck MVONDO <franck.mvondo@outlook.fr>
2023-03-20 08:43:02 +0100

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
+#/usr/bin/env python3
+# coding: utf-8
+
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-async def Home():
+async def Home() -> dict:
+    return {"Home": "Hello World"}
+
+
+@app.get("/cities_recommendation/")
+async def cities_recommendation(
+    department_code: str, 
+    area: float, 
+    rent: float) -> dict:
     return {"Home": "Hello World"}
