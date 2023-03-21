eb72b2e60351417bef4a7848eece6319ab49fe4c
Franck MVONDO <franck.mvondo@outlook.fr>
2023-03-21 02:40:03 +0100

    Update database and main

--- src/database.py
+++ src/database.py
@@ -2,12 +2,12 @@
 # coding: utf-8
 
 
-from typing import Any
-
+from bs4 import BeautifulSoup
 from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
-
+from typing import Any
 from unidecode import unidecode
+
 
 import io
 import pandas as pd
@@ -28,75 +28,121 @@ Base.query = db_session.query_property()
 
 class Cities(Base):
     __tablename__ = "cities"
-    city_id = Column(Integer, primary_key=True, autoincrement=True)
-    city_id_zone = Column(String)
-    city_insee = Column(String)
-    city_libgeo = Column(String)
-    city_department_code = Column(String)
-    city_average_rent_per_m2 = Column(Float)
-    city_note = Column(Float)
-    city_population = Column(Integer)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    id_zone = Column(String)
+    insee_code = Column(String)
+    libgeo = Column(String)
+    department_code = Column(String)
+    average_rent_per_m2 = Column(Float)
+    note = Column(Float)
+    population = Column(Integer)
+    zip_code = Column(String)
 
     def __init__(
-        self,
-        city_id_zone,
-        city_insee,
-        city_libgeo,
-        city_department_code,
-        city_average_rent_per_m2,
+        self, id_zone, insee_code, libgeo, department_code, average_rent_per_m2
     ):
-        self.city_id_zone = city_id_zone
-        self.city_insee = city_insee
-        self.city_libgeo = city_libgeo
-        self.city_department_code = city_department_code
-        self.city_average_rent_per_m2 = city_average_rent_per_m2
+        self.id_zone = id_zone
+        self.insee_code = insee_code
+        self.libgeo = libgeo
+        self.department_code = department_code
+        self.average_rent_per_m2 = average_rent_per_m2
 
     def __repr__(self):
         return f"""<City(
-            city_id_zone='{self.city_id_zone}',
-            city_insee='{self.city_insee}',
-            city_libgeo='{self.city_libgeo}',
-            city_department_code='{self.city_department_code}',
-            city_average_rent_per_m2='{self.city_average_rent_per_m2}')>"""
+            id_zone='{self.id_zone}',
+            insee_code='{self.insee_code}',
+            libgeo='{self.libgeo}',
+            department_code='{self.department_code}',
+            average_rent_per_m2='{self.average_rent_per_m2}')>"""
 
 
-def get_all_insee() -> pd.DataFrame:
+def get_all_insee_code() -> pd.DataFrame:
     url = "https://www.data.gouv.fr/fr/datasets/r/bc9d5d13-07cc-4d38-8254-88db065bd42b"
     response = requests.get(url)
     decoded_content = response.content.decode("ISO-8859-1")
     df = pd.read_csv(io.StringIO(decoded_content), delimiter=";")
-    df = df.drop("REG", axis=1)
-    df = df.drop("EPCI", axis=1)
-    df = df.drop("TYPPRED", axis=1)
-    df = df.drop("lwr.IPm2", axis=1)
-    df = df.drop("upr.IPm2", axis=1)
-    df = df.drop("R2_adj", axis=1)
-    df = df.drop("nbobs_mail", axis=1)
-    df = df.drop("nbobs_com", axis=1)
+    columns_to_drop = [
+        "REG",
+        "EPCI",
+        "TYPPRED",
+        "lwr.IPm2",
+        "upr.IPm2",
+        "R2_adj",
+        "nbobs_mail",
+        "nbobs_com",
+    ]
+    df = df.drop(columns_to_drop, axis=1)
 
-    df["LIBGEO"] = df["LIBGEO"].str.replace("La ", "")
-    df["LIBGEO"] = df["LIBGEO"].str.replace("Le ", "")
-    df["LIBGEO"] = df["LIBGEO"].str.replace("Les ", "")
-    df["LIBGEO"] = df["LIBGEO"].str.replace("L'", "")
-    df["LIBGEO"] = df["LIBGEO"].str.replace("D'", "d-")
-    df["LIBGEO"] = df["LIBGEO"].str.replace("d'", "d-")
-    df["LIBGEO"] = df["LIBGEO"].str.replace("l'", "l-")
-    df["LIBGEO"] = df["LIBGEO"].str.replace(" ", "-")
+    df["LIBGEO"] = (
+        df["LIBGEO"]
+        .str.replace("La ", "")
+        .str.replace("Le ", "")
+        .str.replace("Les ", "")
+        .str.replace("L'", "")
+        .str.replace("D'", "d-")
+        .str.replace("d'", "d-")
+        .str.replace("l'", "l-")
+        .str.replace(" ", "-")
+    )
+
     df["loypredm2"] = df["loypredm2"].str.replace(",", ".")
     df["INSEE_C"] = df["INSEE_C"].apply(lambda x: "0" + x if len(x) == 4 else x)
     df["loypredm2"] = df["loypredm2"].apply(lambda x: float(x))
 
     df.columns = [
-        "city_id_zone",
-        "city_insee",
-        "city_libgeo",
-        "city_department_code",
-        "city_average_rent_per_m2",
+        "id_zone",
+        "insee_code",
+        "libgeo",
+        "department_code",
+        "average_rent_per_m2",
     ]
     return df
 
 
 def init_db():
     Base.metadata.create_all(engine)
-    df = get_all_insee()
+    df = get_all_insee_code()
     df.to_sql("cities", engine, if_exists="append", index=False)
+
+
+def update_db():
+    cities = Cities.query.all()
+    for city in cities:
+        city_details = get_city_details(city.insee_code)
+        if city_details is not None:
+            city.note = get_note(city.insee_code, city.libgeo)
+            city.zip_code = city_details.get("codesPostaux")[0]
+            city.population = city_details.get("population")
+            db_session.commit()
+
+
+def get_city_details(insee_code: str):
+    city_details: None
+    url = f"https://geo.api.gouv.fr/communes/{insee_code}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        city_details = response.json()
+    return city_details
+
+
+def get_city_detailsEx(department_code: str):
+    city_details = None
+    url = f"https://geo.api.gouv.fr/departements/{department_code}/communes"
+    response = requests.get(url)
+    if response.status_code == 200:
+        city_details = response.json()
+    return city_details
+
+
+def get_note(insee_code: str, libgeo: str):
+    note = None
+    url = f"https://www.bien-dans-ma-ville.fr/{unidecode(libgeo).lower()}-{insee_code}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        note = soup.find("div", {"class": "bloc_notemoyenne"}).find(
+            "div", {"class": "total"}
+        )
+        if note is not None:
+            note = note.text
+    return note
--- src/main.py
+++ src/main.py
@@ -1,40 +1,46 @@
 # /usr/bin/env python3
 # coding: utf-8
 
-from .database import init_db, Cities
+from .database import init_db, get_note, get_city_detailsEx, Cities
 
 from fastapi import FastAPI
+from unidecode import unidecode
 
 
 app = FastAPI()
-init_db()
+
+try:
+    init_db()
+except Exception as e:
+    print(f"Erreur dans la tâche : {e}")
 
 
 @app.get("/")
 async def Home() -> dict:
-    return {"Home": "Hello World"}
+    return {"Hello": "World"}
 
 
 @app.get("/cities_recommendation/")
 async def cities_recommendation(
-    department_code: str,
-    area: float,
-    rent: float,
+    department_code: str, area: float, rent: float
 ) -> list[dict]:
     city_list = []
     rent_per_m2 = rent / area
-    cities = Cities.query.filter(
-        Cities.city_department_code == department_code,
-        Cities.city_average_rent_per_m2 <= rent_per_m2,
-    ).all()
-    for city in cities:
-        city_dict = {
-            "Nom de ville": city.city_libgeo,
-            "Code Postale": city.city_insee,
-            "Loyer moyen": city.city_average_rent_per_m2 * area,
-            "Note globale": city.city_note,
-            "Population": city.city_population,
-        }
-        city_list.append(city_dict)
+
+    citiesEx = get_city_detailsEx(department_code)
+    if citiesEx:
+        for element in citiesEx:
+            city = Cities.query.filter_by(insee_code=element["code"]).first()
+            if city:
+                note = get_note(city.insee_code, city.libgeo)
+                if city.average_rent_per_m2 <= rent_per_m2:
+                    city_dict = {
+                        "Nom de ville": city.libgeo,
+                        "Code Postale": element["codesPostaux"],
+                        "Loyer moyen": city.average_rent_per_m2 * area,
+                        "Note globale": note,
+                        "Population": element["population"],
+                    }
+                    city_list.append(city_dict)
 
     return city_list
